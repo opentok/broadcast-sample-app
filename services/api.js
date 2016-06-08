e4a2c976d551ca89bb9da26867a75a6b6e021a7b
Aaron Rice <adrice727@gmail.com>
2016-06-08 16:21:44 -0700

    Update api service.

--- services/api.js
+++ services/api.js
@@ -12,8 +12,24 @@ const OpenTok = require('opentok');
 const OT = Promise.promisifyAll(new OpenTok(apiKey, apiSecret));
 
 /** Private */
-const defaultTokenOptions = { role: 'publisher' };
+
+/**
+ * Returns options for token creation based on user type
+ * @param {string} userType Host, guest, or viewer
+ */
+const tokenOptions = userType => {
+
+  const role = {
+    host: 'moderator',
+    guest: 'publisher',
+    viewer: 'subscriber'
+  }[userType];
+
+  return { role };
+};
+
 const defaultSessionOptions = { mediaMode: 'routed' };
+let activeSession;
 
 /** Exports */
 
@@ -22,15 +38,44 @@ const defaultSessionOptions = { mediaMode: 'routed' };
  * @param {object} [options]
  * @returns {promise.<object, error>}
  */
-exports.createSession = options =>
-  OT.createSessionAsync(R.defaultTo(defaultSessionOptions)(options));
-
+const createSession = options =>
+  new Promise((resolve, reject) => {
+    OT.createSessionAsync(R.defaultTo(defaultSessionOptions)(options))
+      .then(session => {
+        activeSession = session;
+        resolve(session);
+      })
+      .catch(error => reject(error));
+  });
 
 /**
  * Create an OpenTok token
- * @param {string} sessionId
+ * @param {string} userType Host, guest, or viewer
  * @returns {string}
  */
-exports.createToken = (sessionId, options) =>
-  OT.generateToken(sessionId, R.defaultTo(defaultTokenOptions)(options));
+const createToken = userType => {
+  const sessionId = activeSession.id;
+  OT.generateToken(sessionId, tokenOptions(userType));
+};
 
+/**
+ * Creates an OpenTok session and generates an associated token
+ * @returns {promise.<object, error>}
+ */
+const getCredentials = userType =>
+  new Promise((resolve, reject) => {
+    const token = createToken(tokenOptions(userType));
+    if (!!activeSession) {
+      resolve({ sessionId: activeSession.id, token });
+    } else {
+      createSession()
+        .then(session => { resolve({ sessionId: session.id, token }); })
+        .catch(error => reject(error));
+    }
+  });
+
+module.exports = {
+  createSession,
+  createToken,
+  getCredentials,
+};
