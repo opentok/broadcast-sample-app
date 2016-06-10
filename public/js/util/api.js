073a40221ce622afb118e3fa5815c38dd93087d1
Aaron Rice <adrice727@gmail.com>
2016-06-10 12:27:32 -0700

    Create separate module to wrap api calls.

---
+++ public/js/util/api.js
@@ -0,0 +1,39 @@
+/* eslint-disable object-shorthand */
+(function () {
+
+  var api = {
+    post: function (url, data) {
+
+      var requestHeaders = {
+        /* eslint-disable quote-props */
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+          /* eslint-enable quote-props */
+      };
+
+      var parseJSON = function (response) {
+        return response.json();
+      };
+
+      var params = {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify(data)
+      };
+
+      return new Promise(function (resolve, reject) {
+        fetch(url, params)
+          .then(parseJSON)
+          .then(function (json) {
+            resolve(json);
+          })
+          .catch(function (error) {
+            reject(error);
+          });
+      });
+    }
+  };
+
+  window.api = api;
+
+}());
