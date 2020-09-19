73503974b3dc20bad95eddeacd557d42a62f3b5e
Michael Jolley <mike@sparcapp.io>
2020-09-19 18:12:21 -0500

    Updating repo standards & dependencies. Also added custom layouts & active speaker detection.

--- public/js/broadcast.js
+++ public/js/broadcast.js
@@ -1,13 +1,12 @@
-/* global flowplayer */
 (function () {
 
   /**
    * Get our OpenTok API Key, Session ID, and Token from the JSON embedded
    * in the HTML.
    */
-  var getBroadcastData = function () {
-    var el = document.getElementById('broadcast');
-    var credentials = JSON.parse(el.getAttribute('data'));
+  const getBroadcastData = function () {
+    const el = document.getElementById('broadcast');
+    const credentials = JSON.parse(el.getAttribute('data'));
     el.remove();
     return credentials;
   };
@@ -15,10 +14,10 @@
   /**
    * Update the banner based on the status of the broadcast (active or ended)
    */
-  var updateBanner = function (status) {
+  const updateBanner = function (status) {
 
-    var banner = document.getElementById('banner');
-    var bannerText = document.getElementById('bannerText');
+    const banner = document.getElementById('banner');
+    const bannerText = document.getElementById('bannerText');
 
     if (status === 'active') {
       banner.classList.add('hidden');
@@ -29,50 +28,36 @@
     }
   };
 
-  var play = function (source) {
+  const play = function (source) {
 
     updateBanner('active');
 
-    flowplayer('#videoContainer', {
-      splash: false,
-      embed: false,
-      ratio: 9 / 16,
-      autoplay: false,
-      clip: {
-        autoplay: false,
-        live: true,
-        hlsjs: {
-          // listen to hls.js ERROR
-          listeners: ['hlsError'],
-        },
-        sources: [{
-          type: 'application/x-mpegurl',
-          src: source
-        }]
-      }
-    }).on('hlsError', function (e, api, error) {
-
-      // Broadcast end
-      if (error.type === 'networkError' && error.details === 'levelLoadError') {
-        api.stop();
-        updateBanner('ended');
-        document.getElementById('videoContainer').classList.add('hidden');
-      }
-    });
+    const video = document.getElementById('video');
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(source);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play();
+      });
+    }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = source;
+      video.addEventListener('loadedmetadata', function () {
+        video.play();
+      });
+    }
   };
 
-  var init = function () {
-
-    var broadcast = getBroadcastData();
+  const init = function () {
+    const broadcast = getBroadcastData();
     if (broadcast.availableAt <= Date.now()) {
       play(broadcast.url);
     } else {
       setTimeout(function () { play(broadcast.url); },
         broadcast.availableAt - Date.now());
     }
-
   };
 
   document.addEventListener('DOMContentLoaded', init);
-
 }());
